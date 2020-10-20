e6cb406d9562fc89be20b18bf07b3b76a2124170
Cory Evans <cory0x45@gmail.com>
2020-10-21 09:50:18 +1300

    started on copper scrap service page

---
+++ js/copper.js
@@ -0,0 +1,31 @@
+var slideIndex = 0
+
+function slideshowNext() {
+	slideIndex = (slideIndex + 1) % getNumberOfSlides()
+	showSlide()
+}
+
+function slideshowPrevious() {
+	slideIndex = (slideIndex - 1) % getNumberOfSlides()
+	showSlide()
+}
+
+function showSlide() {
+	var panels = document.getElementById('slideshow')
+						 .getElementsByClassName('slideshow-panel')
+	for (let i = 0; i < panels.length; i++) {
+		var p = panels[i]
+		p.style.display = 'none'
+	}
+
+	panels[slideIndex].style.display = 'unset'
+}
+
+function getNumberOfSlides() {
+	var slideshow = document.getElementById('slideshow')
+	return slideshow.getElementsByClassName('slideshow-panel').length;
+}
+
+window.onload = () => {
+	showSlide()
+}
